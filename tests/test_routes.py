7a5681a6fc51d84c9b5b1de9b28c20b2ce98af56
saaddbhatti763 <saadbhatti763>
2023-09-01 10:30:24 -0400

    added link taks

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -24,6 +24,8 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -130,10 +132,10 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.get(
          f"{BASE_URL}/{account.id}", content_type="application/json"    
-        )
+    )
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
-        self.assertEqual(data['name'],account.name)
+        self.assertEqual(data['name'], account.name)
 
     def test_acccount_not_found(self):
         resp = self.client.get(f"{BASE_URL}/0")
@@ -157,17 +159,16 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_list_accounts(self):
-       self._create_accounts(5)
-       resp = self.client.get(BASE_URL)
-       self.assertEqual(resp.status_code, status.HTTP_200_OK)
-       data = resp.get_json()
-       self.assertEqual(len(data), 5)
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
 
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
 
     def test_security_headers(self):
         """It should return security headers"""
@@ -181,9 +182,7 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-
-                
-                
+                                
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
