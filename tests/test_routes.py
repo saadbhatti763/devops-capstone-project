ab7b44eb58f0e92efe3cab561aeab8b32427126a
saaddbhatti763 <saadbhatti763>
2023-09-01 10:35:29 -0400

    added link taks

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -131,8 +131,8 @@ class TestAccountService(TestCase):
     def test_get_account(self):
         account = self._create_accounts(1)[0]
         resp = self.client.get(
-         f"{BASE_URL}/{account.id}", content_type="application/json"    
-    )
+        f"{BASE_URL}/{account.id}", content_type="application/json"    
+        )
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(data['name'], account.name)
@@ -144,8 +144,7 @@ class TestAccountService(TestCase):
     def test_update_an_account(self):
         test_account = AccountFactory()
         resp = self.client.post(BASE_URL, json=test_account.serialize())
-        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
-        
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)        
         new_account = resp.get_json()
         new_account["name"] = "Something Known"
         resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
