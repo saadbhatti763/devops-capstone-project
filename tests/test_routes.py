1150a55bd6c5f380981a2b033cbc136721defc61
saaddbhatti763 <saadbhatti763>
2023-09-01 12:01:36 -0400

    added deploy task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -38,7 +38,7 @@ class TestAccountService(TestCase):
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
         talisman.force_https = False
-    
+
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
@@ -131,7 +131,7 @@ class TestAccountService(TestCase):
     def test_get_account(self):
         account = self._create_accounts(1)[0]
         resp = self.client.get(
-        f"{BASE_URL}/{account.id}", content_type="application/json"    
+        f"{BASE_URL}/{account.id}", content_type="application/json"  
         )
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
@@ -144,7 +144,7 @@ class TestAccountService(TestCase):
     def test_update_an_account(self):
         test_account = AccountFactory()
         resp = self.client.post(BASE_URL, json=test_account.serialize())
-        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)   
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)  
         new_account = resp.get_json()
         new_account["name"] = "Something Known"
         resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
